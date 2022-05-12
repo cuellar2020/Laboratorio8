1c9dd8b3de33a40b595c94c2b48f13aeb5a2eae6
cuellar2020 <67763251+cuellar2020@users.noreply.github.com>
2022-05-12 18:03:33 -0400

    Add project files.

---
+++ Laboratorio8/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Laboratorio8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            item01.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+
+            item02.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+
+            item03.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+
+            item04.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+
+            item05.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new GlobalDemo());
+            };
+
+            item06.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
+            item07.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new DemoCss());
+
+            };
+
+
+        }
+    }
+}
